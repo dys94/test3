7db3491c0e7f82b40bac5525d38c512889c95cb1
Andrei <ershov-andrei.@mail.ru>
2023-12-25 19:59:50 +0500

    выполнил

--- dz10.py
+++ dz10.py
@@ -1,6 +1,35 @@
+import pandas as pd
+
+
+
+# Исходный код
+
 import random
+
 lst = ['robot'] * 10
+
 lst += ['human'] * 10
+
 random.shuffle(lst)
-data = pd.DataFrame({'whoAmI':lst})
-data.head()
+
+data = pd.DataFrame({'whoAmI': lst})
+
+print("Исходный DataFrame:")
+
+print(data.head())
+
+
+
+# One hot
+
+one_hot_data = pd.get_dummies(data['whoAmI'])
+
+data_one_hot = pd.concat([data, one_hot_data], axis=1)
+
+data_one_hot = data_one_hot.drop('whoAmI', axis=1)
+
+
+
+print("\nDataFrame после one hot encoding:")
+
+print(data_one_hot.head())
